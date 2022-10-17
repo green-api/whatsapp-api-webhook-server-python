7805b2366fc881fbe721cdac2a12e498e5b5801c
sadiv <sadiv@bk.ru>
2022-10-17 15:52:50 +0300

    fix test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,3 @@
-import time
-import requests
-import asyncio
 import whatsapp_api_webhook_server_python.webhooksHandler as handler
 
 result = None
@@ -9,14 +6,13 @@ class TestClass:
     def test_Started(self):       
         started = True   
         try: 
-            handler.startServer('http://127.0.0.1', 8000, onEvent, False)
+            handler.startServer('127.0.0.1', 8000, onEvent, False)
         except:
             started = False
         assert started == True
 
 def onEvent(webhooksHandler: handler.WebhooksHandler, typeWebhook: str, body):
-    global result
-    result = True
+    pass
 
 def main():
     TestClass.test_Started(TestClass)
