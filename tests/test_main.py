8b73e4785a6fdc235a52e9d737e569859d018aa6
Amele9 <amele090044@gmail.com>
2024-01-13 19:37:37 +0600

    Fixed tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,20 +2,26 @@ import whatsapp_api_webhook_server_python.webhooksHandler as handler
 
 result = None
 
-class TestClass: 
-    def test_Started(self):       
-        started = True   
-        try: 
+
+class TestClass:
+    def test_Started(self):
+        started = True
+        try:
             handler.startServer('127.0.0.1', 8000, onEvent, False)
-        except:
+        except Exception as error:
+            print(error)
+
             started = False
-        assert started == True
+        assert started
+
 
 def onEvent(webhooksHandler: handler.WebhooksHandler, typeWebhook: str, body):
     pass
 
+
 def main():
     TestClass.test_Started(TestClass)
+
 
 if __name__ == "__main__":
     main()
